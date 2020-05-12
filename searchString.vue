f94037d730f082ce520e330aa194b02546ad67d5
Vanya <polovko99v@gmail.com>
2020-05-12 13:59:52 +0300

    v1

---
+++ searchString.vue
@@ -0,0 +1,33 @@
+<template>
+    <form @submit.prevent>
+        From date <input type="date" v-model="searchString.fromDate">
+        <input type="button" value="Шукати" @click="search()">
+        <input type="button" value="Очистити пошук" @click="clear()">
+    </form>
+</template>
+<script>
+import {mapMutations} from 'vuex';
+export default {
+    data(){
+        return{
+           searchString:{
+               fromDate:""
+           }
+        }
+    },
+    methods:{
+        ...mapMutations(["setSerchString"]),
+         clear(){
+            this.searchString.fromDate="";
+            this.setSerchString(this.searchString);
+        },
+        search(){            
+            this.setSerchString(this.searchString);
+        }
+       
+    }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ store.js
@@ -0,0 +1,27 @@
+import Vuex from 'vuex';
+import Vue from "vue";
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: {
+        messages: [],
+        photos: [],
+        searchString: "",
+        formVisible: false,
+        formPassenger: {},
+        formNewMode: true
+    },
+    getters: {
+        filtredPhotos(state) {
+            let result = state.photos;
+            if (state.searchString)
+                result = result.filter(photo =>
+                    photo.date>state.searchString
+                );
+            return result;
+        },
+
+    }
+});
+export default store;
